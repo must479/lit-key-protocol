700f09f1002e6cc9311e6d414d2396031109609f
Anson <anson@ansons-mbp-2.home>
2022-07-06 17:42:05 +0100

    add test case

--- test/index.js
+++ test/index.js
@@ -2,11 +2,11 @@ import { encodeDIDWithLit } from '../dist/index.js';
 import { assert } from "console";
 import { expect } from 'chai';
 
-describe('test', () => {
+describe('encodeDIDWithLit', () => {
 
     const PKP_PUBLIC_KEY = '30eceb963993d467ca197f3fd9fe3073b8b224ac2c9068d9a9caafcd5e20cf983';
 
-    it('encodedDID should be a string', async function () {
+    it('should be a string', async function () {
 
         const encodedDID = await encodeDIDWithLit({
             pkpPublicKey: PKP_PUBLIC_KEY
@@ -15,7 +15,7 @@ describe('test', () => {
         expect(encodedDID).to.be.a.string;
     });
 
-    it('encodedDID should have prefix "did:key:"', async function () {
+    it('should have prefix "did:key:"', async function () {
 
         const encodedDID = await encodeDIDWithLit({
             pkpPublicKey: PKP_PUBLIC_KEY
@@ -23,8 +23,14 @@ describe('test', () => {
 
         const arr = encodedDID.split(':');
 
-        console.log(arr);
-
         expect(arr).to.include.members(['key', 'did'])
     });
+
+    it('should return "did:key:zQ3shfdufQXuqrY4TYSGuk2W7VdDUzzFTSnTWSLA1FhmhdMce" with this public key "30eceb963993d467ca197f3fd9fe3073b8b224ac2c9068d9a9caafcd5e20cf983"', async () => {
+        const encodedDID = await encodeDIDWithLit({
+            pkpPublicKey: PKP_PUBLIC_KEY
+        });
+
+        expect(encodedDID).to.equal('did:key:zQ3shfdufQXuqrY4TYSGuk2W7VdDUzzFTSnTWSLA1FhmhdMce');
+    })
 })
